870aadb83c7cc9c1c41d12ba66ed8f2a76a04f6d
RMYanenko <yanenko.r.m@gmail.com>
2020-08-21 15:26:10 +0300

    location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,10 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const appKey = '1513ad5284e73805a5e244ce08c6e71c';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,41 +12,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getDate();
+    getLocationData();
   }
 
-  void getLocation() async {
+  getLocationData() async {
     Location location = Location();
+
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
 
-  void getDate() async {
-    http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-      // var tempDate = jsonDecode(data)['main']['temp'];
-      // var idDate = jsonDecode(data)['weather'][0]['id'];
-      // var nameDate = jsonDecode(data)['name'];
+    NetworkHelper networkHelper = NetworkHelper(
+      url:
+          'https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$appKey',
+    );
 
-      // print(tempDate);
-      // print(idDate);
-      // print(nameDate);
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(locationWeather: weatherData,),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    getDate();
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.blueGrey,
+          size: 150.0,
+        ),
+      ),
+    );
   }
 }
